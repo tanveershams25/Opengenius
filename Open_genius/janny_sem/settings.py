f26ac7d4d7ea111dc146987fd18ab3621ef6067f
Amit Kumar <khuttaamitkumar@gmail.com>
2026-03-05 23:31:41 +0530

    updated requirements

--- Open_genius/janny_sem/settings.py
+++ Open_genius/janny_sem/settings.py
@@ -132,6 +132,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files configuration
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'App', 'static')
--- Open_genius/janny_sem/wsgi.py
+++ Open_genius/janny_sem/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Open_genius.janny_sem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'janny_sem.settings')
 
 application = get_wsgi_application()
